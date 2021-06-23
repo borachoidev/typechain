42586748bf4908e5b8f35e7ef5f6bd5bb9bda7c6
september21st <september21st@users.noreply.github.com>
2021-06-23 16:52:59 +0900

    feat: Blockchain creating a  block

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,26 @@
-class Human{
-    public name: string;
-    public age: number;
-    public gender: string;
-    constructor(name: string, age:number, gender: string){
-        this.name = name;
-        this.age = age;
-        this.gender = gender;
-    }
+import { defaultCipherList } from 'constants';
+
+class Block{
+    public index:number;
+    public hash: string;
+    public previousHash: string;
+    public data: string;
+    public timestamp: number;
+    constructor( 
+        index:number,
+        hash: string,
+        previousHash: string,
+        data: string,
+        timestamp: number){
+            this.index = index;
+            this.hash = hash;
+            this.previousHash = previousHash;
+            this.data= data;
+            this.timestamp =timestamp;
+        }
 }
 
-const bora = new Human("Bora",18,"female");
-
-const sayHi =(person:Human): string => {
-    return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}`;
-}
-
-sayHi(bora);
+const genesisBlock:Block = new Block(0,"20202020202020","","hello",123123);
+let blockchain:[Block] = [genesisBlock];
+console.log(blockchain);
 export {};
