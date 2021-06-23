416302b687eebdb6e2abf8c62f5c7200d3bdf9e6
september21st <september21st@users.noreply.github.com>
2021-06-23 17:18:09 +0900

    feat: Create a Block

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { defaultCipherList } from 'constants';
+import * as CryptoJS from "crypto-js";
 
 class Block{
     public index:number;
@@ -6,6 +6,15 @@ class Block{
     public previousHash: string;
     public data: string;
     public timestamp: number;
+    
+    static calculateBlockHash = (
+        index: number,
+        previousHash: string,
+        timestamp: number,
+        data: string
+    ): string =>
+        CryptoJS.SHA256(index + previousHash + timestamp +data).toString();
+    
     constructor( 
         index:number,
         hash: string,
@@ -21,6 +30,12 @@ class Block{
 }
 
 const genesisBlock:Block = new Block(0,"20202020202020","","hello",123123);
-let blockchain:[Block] = [genesisBlock];
-console.log(blockchain);
+let blockchain:Block[] = [genesisBlock];
+
+const getBlockchain = (): Block[] => blockchain;
+
+const getLatestBblock = (): Block => blockchain[blockchain.length-1];
+
+const getNewTimestamp = (): number => Math.round(new Date().getTime()/1000);
+
 export {};
