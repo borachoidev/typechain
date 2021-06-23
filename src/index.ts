dab8f82eccb3d38f9377c22bec23fc298cf052cd
september21st <september21st@users.noreply.github.com>
2021-06-23 16:22:07 +0900

    feat: Interfaces on Typescript
    interface를 통해서 Object의 속성값의 type을 설정할 수 있다.
    interface는 typescript에서만 작동한다.(JS로 변환 시켰을때에는 없어짐)

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,16 @@
-const sayHi =(name: string, age: number, gender: string): string => {
-    return `Hello ${name}, you are ${age}, you are a ${gender}`;
+interface Human{
+    name:string;
+    age:number;
+    gender: string;
+}
+const person = {
+    name:"bora",
+    gender:"female",
+    age:22
+};
+const sayHi =(person:Human): string => {
+    return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}`;
 }
 
-sayHi("bora",20,"female");
+sayHi(person);
 export {};
